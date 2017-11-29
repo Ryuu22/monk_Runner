cec74935def6a000e78a43bd83a05801814d5db6
Daniel Carmona <DaniiCarmona22@gmail.com>
2017-11-29 13:21:18 +0100

    Added movement

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,29 +6,43 @@ public class Player : MonoBehaviour {
 
 
     [SerializeField] private bool running;
+    [SerializeField] private Vector2 speed;
+    private Rigidbody2D rb;
+    public Animator anim;
+
     // Use this for initialization
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         running = false;
+        speed.x = 10.0f;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) MakePlayerRun();
+        if (Input.GetKeyUp(KeyCode.RightArrow)) MakePlayerStop();
+
         if (running)
         {
-
+            rb.AddForce(speed);
         }
+
+        //Animation
+        anim.SetBool("running", running);
+
     }
 
-    void MakePlayerRun()
+    void MakePlayerRun()//switches on the boolean and activates the animation
     {
         running = true;
     }
 
     void MakePlayerStop()
     {
-
+        running = false;
+        rb.velocity = Vector3.zero;
     }
 
 }
