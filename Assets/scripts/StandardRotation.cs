fecd51af94aa755cf5c3c64807cf2617de4f7d09
Daniel Carmona <DaniiCarmona22@gmail.com>
2017-12-06 19:36:32 +0100

    Coded rotation function

--- Assets/scripts/StandardRotation.cs
+++ Assets/scripts/StandardRotation.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class StandardRotation : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    // Use this for initialization
+    [SerializeField] float speed;
+    [SerializeField] float time;
+    [SerializeField] float timeCounter;
+
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (timeCounter < time) timeCounter += Time.deltaTime;
+        else
+        {
+            timeCounter = 0;
+            speed *= -1;
+        }
+        this.transform.Rotate(new Vector3(0, 0, speed));
 	}
 }
