3b47862bb49405a1827eba8133a4e54fda202dbb
Daniel Carmona <DaniiCarmona22@gmail.com>
2018-01-10 22:50:44 +0100

    Edited how parallax works

---
+++ Assets/scripts/FollowXAxis.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowXAxis : MonoBehaviour {
+
+    public Transform cameraGO;
+    public float time;
+
+    Vector2 newPosition;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        cameraGO = GameObject.FindObjectOfType<Player>().transform;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        this.transform.position = new Vector3(cameraGO.position.x,this.transform.position.y, this.transform.position.z);
+
+    }
+}
